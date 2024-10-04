d48c6bc61754f0558741ff50399654d3a048923e
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2024-10-05 04:50:26 +0800

    Remove additional minifier

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// next.config.js
 const withPWA = require("next-pwa")({
     dest: "public",
     disable: process.env.NODE_ENV === "development",
@@ -5,11 +6,8 @@ const withPWA = require("next-pwa")({
     skipWaiting: true,
 });
 
-const { default: classnamesMinifier } = require('@nimpl/classnames-minifier');
-const { PHASE_PRODUCTION_SERVER, PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
 /** @type {import('next').NextConfig} */
-const baseNextConfig = {
+const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
@@ -17,18 +15,4 @@ const baseNextConfig = {
     },
 };
 
-// Export a function to handle both PWA and classnames minifier
-module.exports = (phase) => {
-    const isProductionServer = phase === PHASE_PRODUCTION_SERVER;
-    const isDevelopmentServer = phase === PHASE_DEVELOPMENT_SERVER;
-
-    // Classnames Minifier Configuration
-    const minifierConfig = classnamesMinifier({
-        prefix: '_', // Minified classname prefix
-        reservedNames: ['_en', '_de'], // List of classnames that should not be minified
-        disabled: isDevelopmentServer || isProductionServer, // Disable minifier in development and production server
-    })(baseNextConfig);
-
-    // Combine with PWA
-    return withPWA(minifierConfig);
-};
+module.exports = withPWA(nextConfig);
